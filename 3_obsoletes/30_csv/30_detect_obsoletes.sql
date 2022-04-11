872c98baf7753e3324a4b7967c41e0148a598cd6
Pascal Alfadian Nugroho <pascalalfadian@live.com>
2022-04-11 16:58:05 +0700

    Sample query for loading supported version to array of struct

--- 3_obsoletes/30_csv/30_detect_obsoletes.sql
+++ 3_obsoletes/30_csv/30_detect_obsoletes.sql
@@ -4,3 +4,6 @@ LANGUAGE js AS r"""
 	return false
 """;
 SELECT isSupportedSingle('a', 'b', 'c') FROM `httparchive.technologies.2022_02_01_*` LIMIT 10;
+
+# Loading of library to array of struct
+SELECT ARRAY(SELECT AS STRUCT * FROM `avian-current-603.InternationalWebsiteSurvey.technologies`);
