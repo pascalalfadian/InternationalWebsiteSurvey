cf74edbad9b5562a4dd3f05e415d9b732b2c8121
Pascal Alfadian Nugroho <pascalalfadian@live.com>
2022-04-12 11:47:28 +0700

    Integrating table to UDF
    Still have error: Access Denied: Table httparchive:technologies.2022_02_01_*: User does not have permission to query table httparchive:technologies.2022_02_01_*.

--- 3_obsoletes/30_csv/30_detect_obsoletes.sql
+++ 3_obsoletes/30_csv/30_detect_obsoletes.sql
@@ -1,9 +1,9 @@
-CREATE TEMP FUNCTION isSupportedSingle(currentVersion STRING, supportedVersion STRING, technologyName STRING)
+CREATE TEMP FUNCTION isSupportedSingle(currentVersion STRING, supportedVersion STRING, technologyName STRING, supportedVersions ARRAY<STRUCT<INT64,STRING,INT64,INT64,INT64,STRING,STRING,STRING,STRING>>)
 RETURNS BOOLEAN
 LANGUAGE js AS r"""
 	return false
 """;
-SELECT isSupportedSingle('a', 'b', 'c') FROM `httparchive.technologies.2022_02_01_*` LIMIT 10;
+SELECT isSupportedSingle('a', 'b', 'c',ARRAY(SELECT AS STRUCT * FROM `avian-current-603.InternationalWebsiteSurvey.technologies`)) FROM `httparchive.technologies.2022_02_01_*` LIMIT 10;
 
 # Loading of library to array of struct
 SELECT ARRAY(SELECT AS STRUCT * FROM `avian-current-603.InternationalWebsiteSurvey.technologies`);
