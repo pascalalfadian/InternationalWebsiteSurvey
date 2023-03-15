320a599f4116a6bb24f139310c67567e15197aba
Pascal Alfadian Nugroho <pascalalfadian@live.com>
2023-03-15 14:29:01 +0700

    Problem solved. Next: more lines

--- 3_obsoletes/30_csv/30_detect_obsoletes.sql
+++ 3_obsoletes/30_csv/30_detect_obsoletes.sql
@@ -1,11 +1,11 @@
-CREATE TEMP FUNCTION isSupportedSingle(currentVersion STRING, supportedVersion STRING, technologyName STRING, supportedVersions ARRAY<STRUCT<STRING,INT64,INT64,INT64,STRING,STRING,STRING,STRING>>)
+CREATE TEMP FUNCTION isSupportedSingle(currentVersion STRING, supportedVersion STRING, technologyName STRING, supportedVersions ARRAY<STRUCT<name STRING,num_sites INT64,num_unversioned INT64,num_versioned INT64,website STRING,min_supported_version STRING,min_supported_version_eol STRING,min_supported_version_reference STRING>>)
 RETURNS BOOLEAN
 LANGUAGE js AS r"""
-    if (!current_version || !supported_version || supported_version === '?') {
+    if (!currentVersion || !supportedVersion || supportedVersion === '?') {
         return NULL;
     }
 """;
 SELECT isSupportedSingle('7.4', '>= 8.1', 'PHP', ARRAY(SELECT AS STRUCT * FROM `avian-current-603.InternationalWebsiteSurveyUS.technologies`)) FROM `httparchive.technologies.2023_01_01_*` LIMIT 10;
 
 # Loading of library to array of struct
-SELECT ARRAY(SELECT AS STRUCT * FROM `avian-current-603.InternationalWebsiteSurvey.technologies`);
+SELECT ARRAY(SELECT AS STRUCT * FROM `avian-current-603.InternationalWebsiteSurveyUS.technologies`);
