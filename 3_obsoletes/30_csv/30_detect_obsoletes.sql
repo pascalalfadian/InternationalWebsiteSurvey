d229790cdc485d6b39381ca1c3625463a9bc5a30
Pascal Alfadian Nugroho <pascalalfadian@live.com>
2023-03-15 14:13:48 +0700

    Trying one line
    Stil had error:
    
    Access Denied: Table httparchive:technologies.2023_01_01_*: User does not have permission to query table httparchive:technologies.2023_01_01_*, or perhaps it does not exist in location asia-southeast2. at [8:1]

--- 3_obsoletes/30_csv/30_detect_obsoletes.sql
+++ 3_obsoletes/30_csv/30_detect_obsoletes.sql
@@ -1,9 +1,11 @@
-CREATE TEMP FUNCTION isSupportedSingle(currentVersion STRING, supportedVersion STRING, technologyName STRING, supportedVersions ARRAY<STRUCT<INT64,STRING,INT64,INT64,INT64,STRING,STRING,STRING,STRING>>)
+CREATE TEMP FUNCTION isSupportedSingle(currentVersion STRING, supportedVersion STRING, technologyName STRING, supportedVersions ARRAY<STRUCT<STRING,INT64,INT64,INT64,STRING,STRING,STRING,STRING>>)
 RETURNS BOOLEAN
 LANGUAGE js AS r"""
-	return false
+    if (!current_version || !supported_version || supported_version === '?') {
+        return NULL;
+    }
 """;
-SELECT isSupportedSingle('a', 'b', 'c',ARRAY(SELECT AS STRUCT * FROM `avian-current-603.InternationalWebsiteSurvey.technologies`)) FROM `httparchive.technologies.2022_02_01_*` LIMIT 10;
+SELECT isSupportedSingle('7.4', '>= 8.1', 'PHP', ARRAY(SELECT AS STRUCT * FROM `avian-current-603.InternationalWebsiteSurvey.technologies`)) FROM `httparchive.technologies.2023_01_01_*` LIMIT 10;
 
 # Loading of library to array of struct
 SELECT ARRAY(SELECT AS STRUCT * FROM `avian-current-603.InternationalWebsiteSurvey.technologies`);
