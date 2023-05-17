35bc4c18b319f67f2eaf8dca8ed2b395313faf7a
Pascal Alfadian Nugroho <pascalalfadian@live.com>
2023-05-17 16:24:30 +0700

    More code

--- 3_obsoletes/30_csv/30_detect_obsoletes.sql
+++ 3_obsoletes/30_csv/30_detect_obsoletes.sql
@@ -1,7 +1,16 @@
 CREATE TEMP FUNCTION isSupportedSingle(currentVersion STRING, supportedVersion STRING, technologyName STRING, supportedVersions ARRAY<STRUCT<name STRING,num_sites INT64,num_unversioned INT64, num_versioned INT64,website STRING,min_supported_version STRING,min_supported_version_eol STRING, supported_version_eol STRING>>)
 RETURNS BOOLEAN
 LANGUAGE js AS r"""
-	return false
+	if (currentVersion === '' || supportedVersion === '' || supportedVersion === '?') {
+		return NULL;
+	}
+
+	if (currentVersion === '.' || !isNumber(currentVersion.replace('.', ''))) {
+		console.err('WARN: unidentified current version after separation: ' + current_version + ' (supported: ' + technologyName + ' ' + supported_version + '). Returning null');
+		return NULL;
+	}
+
+	return FALSE;
 """;
 SELECT isSupportedSingle('a', 'b', 'c',ARRAY(SELECT AS STRUCT * FROM `avian-current-603.InternationalWebsiteSurveyUS.technologies`)) FROM `httparchive.technologies.2022_02_01_*` LIMIT 10;
 
