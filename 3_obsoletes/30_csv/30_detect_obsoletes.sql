c25b765e4bb3ec752f0f30a4117bf39acc42f87b
Pascal Alfadian Nugroho <pascalalfadian@live.com>
2023-05-17 15:19:43 +0700

    More > Query settings > Advanced > Region set to US

--- 3_obsoletes/30_csv/30_detect_obsoletes.sql
+++ 3_obsoletes/30_csv/30_detect_obsoletes.sql
@@ -1,9 +1,9 @@
-CREATE TEMP FUNCTION isSupportedSingle(currentVersion STRING, supportedVersion STRING, technologyName STRING, supportedVersions ARRAY<STRUCT<INT64,STRING,INT64,INT64,INT64,STRING,STRING,STRING,STRING>>)
+CREATE TEMP FUNCTION isSupportedSingle(currentVersion STRING, supportedVersion STRING, technologyName STRING, supportedVersions ARRAY<STRUCT<name STRING,num_sites INT64,num_unversioned INT64, num_versioned INT64,website STRING,min_supported_version STRING,min_supported_version_eol STRING, supported_version_eol STRING>>)
 RETURNS BOOLEAN
 LANGUAGE js AS r"""
 	return false
 """;
-SELECT isSupportedSingle('a', 'b', 'c',ARRAY(SELECT AS STRUCT * FROM `avian-current-603.InternationalWebsiteSurvey.technologies`)) FROM `httparchive.technologies.2022_02_01_*` LIMIT 10;
+SELECT isSupportedSingle('a', 'b', 'c',ARRAY(SELECT AS STRUCT * FROM `avian-current-603.InternationalWebsiteSurveyUS.technologies`)) FROM `httparchive.technologies.2022_02_01_*` LIMIT 10;
 
 # Loading of library to array of struct
-SELECT ARRAY(SELECT AS STRUCT * FROM `avian-current-603.InternationalWebsiteSurvey.technologies`);
+SELECT ARRAY(SELECT AS STRUCT * FROM `avian-current-603.InternationalWebsiteSurveyUS.technologies`);
