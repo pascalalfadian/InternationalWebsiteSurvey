f290e949ef2c4d402eb0ae7c5be8363ed9b4e013
Pascal Alfadian Nugroho <pascalalfadian@live.com>
2023-07-13 10:44:00 +0700

    More codes. isNumber() not found. Where was it???

--- 3_obsoletes/30_csv/30_detect_obsoletes.sql
+++ 3_obsoletes/30_csv/30_detect_obsoletes.sql
@@ -1,15 +1,28 @@
 CREATE TEMP FUNCTION isSupportedSingle(currentVersion STRING, supportedVersion STRING, technologyName STRING, supportedVersions ARRAY<STRUCT<name STRING,num_sites INT64,num_unversioned INT64,num_versioned INT64,website STRING,min_supported_version STRING,min_supported_version_eol STRING,min_supported_version_reference STRING>>)
 RETURNS BOOLEAN
 LANGUAGE js AS r"""
-    if (!currentVersion || !supportedVersion || supportedVersion === '?') {
-        return NULL;
-    }
-	if (currentVersion === '.' || !isNumber(currentVersion.replace('.', ''))) {
-		console.err('WARN: unidentified current version after separation: ' + current_version + ' (supported: ' + technologyName + ' ' + supported_version + '). Returning null');
+	if (!currentVersion || !supportedVersion || supportedVersion === '?') {
 		return NULL;
 	}
+	if (currentVersion === '.' || !isNumber(currentVersion.replace('.', ''))) {
+		console.err('WARN: unidentified current version after separation: ' + currentVersion + ' (supported: ' + technologyName + ' ' + supported_version + '). Returning null');
+		return NULL;
+	}
+	if (supportedVersion === '-') {
+		return FALSE;
+	}
+	if (supportedVersion.substr(0, 2) === '>=') {
+		let operator = '>=';
+		let operand = supportedVersion.substr(2).trim();
+	} else if (supportedVersion.substr(0, 1) === '>') {
+		let operator = '>';
+		let operand = supportedVersion.substr(1).trim();
+	} else {
+		let operator = '=';
+		let operand = supportedVersion;
+	}
 
-	return FALSE;
+	return TRUE;
 """;
 SELECT isSupportedSingle('7.4', '>= 8.1', 'PHP', ARRAY(SELECT AS STRUCT * FROM `avian-current-603.InternationalWebsiteSurveyUS.technologies`)) FROM `httparchive.technologies.2023_01_01_*` LIMIT 10;
 
