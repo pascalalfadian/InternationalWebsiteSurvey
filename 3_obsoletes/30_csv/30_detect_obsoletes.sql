99bb27ac6b81e3b0d6240db6b21538cd1b916da8
Pascal Alfadian Nugroho <pascalalfadian@live.com>
2023-07-13 14:24:22 +0700

    Not sure why isNumber now not available, so hardcode.
    Also, return string as no debugging output available.

--- 3_obsoletes/30_csv/30_detect_obsoletes.sql
+++ 3_obsoletes/30_csv/30_detect_obsoletes.sql
@@ -1,15 +1,14 @@
 CREATE TEMP FUNCTION isSupportedSingle(currentVersion STRING, supportedVersion STRING, technologyName STRING, supportedVersions ARRAY<STRUCT<name STRING,num_sites INT64,num_unversioned INT64,num_versioned INT64,website STRING,min_supported_version STRING,min_supported_version_eol STRING,min_supported_version_reference STRING>>)
-RETURNS BOOLEAN
+RETURNS STRING
 LANGUAGE js AS r"""
 	if (!currentVersion || !supportedVersion || supportedVersion === '?') {
-		return NULL;
+		return 'NULL';
 	}
-	if (currentVersion === '.' || !isNumber(currentVersion.replace('.', ''))) {
-		console.err('WARN: unidentified current version after separation: ' + currentVersion + ' (supported: ' + technologyName + ' ' + supported_version + '). Returning null');
-		return NULL;
+	if (currentVersion === '.' || !(/^[\d.]+$/.test(currentVersion))) {
+		return 'WARN: unidentified current version after separation: ' + currentVersion + ' (supported: ' + technologyName + ' ' + supportedVersion + ')';
 	}
 	if (supportedVersion === '-') {
-		return FALSE;
+		return "FALSE";
 	}
 	if (supportedVersion.substr(0, 2) === '>=') {
 		let operator = '>=';
@@ -22,7 +21,7 @@ LANGUAGE js AS r"""
 		let operand = supportedVersion;
 	}
 
-	return TRUE;
+	return "TRUE";
 """;
 SELECT isSupportedSingle('7.4', '>= 8.1', 'PHP', ARRAY(SELECT AS STRUCT * FROM `avian-current-603.InternationalWebsiteSurveyUS.technologies`)) FROM `httparchive.technologies.2023_01_01_*` LIMIT 10;
 
