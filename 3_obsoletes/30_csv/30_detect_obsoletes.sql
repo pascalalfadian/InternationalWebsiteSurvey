64fa935c59ccdae049488d18b25232539a45897e
Pascal Alfadian Nugroho <pascalalfadian@live.com>
2023-07-17 10:10:47 +0700

    First working result. Next: check WARN and FAILs

--- 3_obsoletes/30_csv/30_detect_obsoletes.sql
+++ 3_obsoletes/30_csv/30_detect_obsoletes.sql
@@ -2,10 +2,10 @@ CREATE OR REPLACE FUNCTION InternationalWebsiteSurveyUS.isSupported(currentVersi
 RETURNS STRING
 LANGUAGE js AS r"""
 	if (!currentVersion || !supportedVersion || supportedVersion === '?') {
-		return null;
+		return 'Non-conclusive';
 	}
   if (currentVersion.indexOf('alpha') !== -1 || currentVersion.indexOf('beta') !== -1 || supportedVersion === '-') {
-    return 'FALSE';
+    return 'Unsupported';
   }
   if (currentVersion === 'squeeze' && technologyName === 'Debian') {
     return "WARN: codename 'squeeze' renamed to 6.0 for Debian";
@@ -37,7 +37,7 @@ LANGUAGE js AS r"""
 		arrCurrentVersion = currentVersion.split('.');
 		for (let j = 0; j < arrOperand.length; j++) {
 			if (j >= arrCurrentVersion.length) {
-				return null;
+				return 'Non-conclusive';
 			}
 			if (!(/^\d+$/.test(arrOperand[j])) || !(/^\d+$/.test(arrCurrentVersion[j]))) {
 				return 'FAIL: Error while checking current version ' + currentVersion + ' vs supported version ' + versionCheck + ' (non integer version: ' + arrOperand[j] + ' or ' + arrCurrentVersion[j] + ')';
@@ -47,19 +47,19 @@ LANGUAGE js AS r"""
 				if (arrOperand[j] === arrCurrentVersion[j]) {
 					// void, pass
 				} else {
-					return 'FALSE';
+					return 'Unsupported';
 				}
 				break;
 			case '>':
 			case '>=':
 				if (operator === '>' && parseInt(arrCurrentVersion[j]) > parseInt(arrOperand[j])) {
-					return 'TRUE';
+					return 'Supported';
 				} else if (operator === '>=' && parseInt(arrCurrentVersion[j]) >= parseInt(arrOperand[j])) {
-					return 'TRUE';
+					return 'Supported';
         } else if (parseInt(arrCurrentVersion[j]) === parseInt(arrOperand[j])) {
 					// void, check next version component
 				} else {
-					return 'FALSE';
+					return 'Unsupported';
 				}
 				break;
 			}
